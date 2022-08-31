37d8fc458851c746c8a3e91f4667d340349e8ac9
yazui <yazui.zag@gmail.com>
2022-08-31 23:43:19 +0200

    Create PaymentResponse.java

---
+++ src/main/java/com/tube/hosting/java/tube/objects/PaymentResponse.java
@@ -0,0 +1,23 @@
+package com.tube.hosting.java.tube.objects;
+
+import org.jetbrains.annotations.NotNull;
+
+public class PaymentResponse extends ResponsableObject {
+    private final @NotNull String link;
+    private final @NotNull String paymentMethodId;
+
+    public PaymentResponse(@NotNull String link, @NotNull String paymentMethodId) {
+        this.link = link;
+        this.paymentMethodId = paymentMethodId;
+    }
+
+    @NotNull
+    public String getLink() {
+        return link;
+    }
+
+    @NotNull
+    public String getPaymentMethodId() {
+        return paymentMethodId;
+    }
+}
