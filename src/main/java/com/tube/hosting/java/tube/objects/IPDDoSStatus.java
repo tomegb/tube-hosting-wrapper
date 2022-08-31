74153ae5ac5014f87a047bebfdf556415994ec1a
yazui <yazui.zag@gmail.com>
2022-08-31 23:43:11 +0200

    Update IPDDoSStatus.java

--- src/main/java/com/tube/hosting/java/tube/objects/IPDDoSStatus.java
+++ src/main/java/com/tube/hosting/java/tube/objects/IPDDoSStatus.java
@@ -2,7 +2,24 @@ package com.tube.hosting.java.tube.objects;
 
 import org.jetbrains.annotations.NotNull;
 
-public record IPDDoSStatus(@NotNull Layer4 layer4, @NotNull Layer7 layer7) {
+public class IPDDoSStatus extends ResponsableObject {
+  private final @NotNull Layer4 layer4;
+  private final @NotNull Layer7 layer7;
+
+  public IPDDoSStatus(@NotNull Layer4 layer4, @NotNull Layer7 layer7) {
+    this.layer4 = layer4;
+    this.layer7 = layer7;
+  }
+
+  @NotNull
+  public Layer4 getLayer4() {
+    return layer4;
+  }
+
+  @NotNull
+  public Layer7 getLayer7() {
+    return layer7;
+  }
 
   public enum Layer4 {
     DYNAMIC,
