cce063aade426be00d44f913bd676a6269ff068f
yazui <yazui.zag@gmail.com>
2022-08-31 23:43:00 +0200

    Create BandwidthResponse.java

---
+++ src/main/java/com/tube/hosting/java/tube/objects/BandwidthResponse.java
@@ -0,0 +1,28 @@
+package com.tube.hosting.java.tube.objects;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BandwidthResponse extends ResponsableObject {
+    private final @NotNull String time;
+    private final long out;
+    private final long in;
+
+    public BandwidthResponse(@NotNull String time, long out, long in) {
+        this.time = time;
+        this.out = out;
+        this.in = in;
+    }
+
+    @NotNull
+    public String getTime() {
+        return time;
+    }
+
+    public long getOut() {
+        return out;
+    }
+
+    public long getIn() {
+        return in;
+    }
+}
