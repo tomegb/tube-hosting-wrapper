e885dae012e2e33d8b8e96e349ec94cfafa57257
Mikka <dev@kxmpetentes.de>
2022-08-31 23:00:24 +0200

    Override toString

--- src/main/java/com/tube/hosting/java/tube/objects/Locale.java
+++ src/main/java/com/tube/hosting/java/tube/objects/Locale.java
@@ -1,6 +1,15 @@
 package com.tube.hosting.java.tube.objects;
 
+import com.google.gson.JsonObject;
+
 public enum Locale {
 
-  DE, EN
+  DE, EN;
+
+  @Override
+  public String toString() {
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.addProperty("locale", name().toUpperCase());
+    return jsonObject.toString();
+  }
 }
