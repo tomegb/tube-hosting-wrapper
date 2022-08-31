2ae4602a78c0bd019a3682f5e594e8422d374d43
yazui <yazui.zag@gmail.com>
2022-08-31 23:42:51 +0200

    Create BalanceChargeRequestBody.java

---
+++ src/main/java/com/tube/hosting/java/tube/objects/BalanceChargeRequestBody.java
@@ -0,0 +1,22 @@
+package com.tube.hosting.java.tube.objects;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BalanceChargeRequestBody extends ResponsableObject {
+    private final int amount;
+    private final @NotNull String method;
+
+    public BalanceChargeRequestBody(int amount, @NotNull String method) {
+        this.amount = amount;
+        this.method = method;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @NotNull
+    public String getMethod() {
+        return method;
+    }
+}
