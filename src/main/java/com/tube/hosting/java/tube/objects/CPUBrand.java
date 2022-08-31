1427123e9744ad8e4e0863b8faf1e964c2d5edbd
yazui <yazui.zag@gmail.com>
2022-08-31 23:43:08 +0200

    Create CPUBrand.java

---
+++ src/main/java/com/tube/hosting/java/tube/objects/CPUBrand.java
@@ -0,0 +1,13 @@
+package com.tube.hosting.java.tube.objects;
+
+import org.jetbrains.annotations.NotNull;
+
+public enum CPUBrand {
+    INTEL, AMD;
+
+    @NotNull
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
+}
