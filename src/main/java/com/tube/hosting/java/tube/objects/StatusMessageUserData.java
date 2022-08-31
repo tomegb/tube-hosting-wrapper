7851ccd7ef560d43277b5e60ea5e09c00fcc7648
yazui <yazui.zag@gmail.com>
2022-08-31 23:43:21 +0200

    Update StatusMessageUserData.java

--- src/main/java/com/tube/hosting/java/tube/objects/StatusMessageUserData.java
+++ src/main/java/com/tube/hosting/java/tube/objects/StatusMessageUserData.java
@@ -1,5 +1,16 @@
 package com.tube.hosting.java.tube.objects;
 
-public record StatusMessageUserData(String readTime) {
+import org.jetbrains.annotations.NotNull;
 
+public class StatusMessageUserData extends ResponsableObject {
+    private final @NotNull String readTime;
+
+    public StatusMessageUserData(@NotNull String readTime) {
+        this.readTime = readTime;
+    }
+
+    @NotNull
+    public String getReadTime() {
+        return readTime;
+    }
 }
