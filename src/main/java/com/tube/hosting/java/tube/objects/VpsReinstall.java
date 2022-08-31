f69277c9404c6562cc545e226ca04c687e78a307
yazui <yazui.zag@gmail.com>
2022-08-31 23:43:24 +0200

    Update VpsReinstall.java

--- src/main/java/com/tube/hosting/java/tube/objects/VpsReinstall.java
+++ src/main/java/com/tube/hosting/java/tube/objects/VpsReinstall.java
@@ -2,6 +2,22 @@ package com.tube.hosting.java.tube.objects;
 
 import org.jetbrains.annotations.NotNull;
 
-public record VpsReinstall(int osId, @NotNull String password, @NotNull String hostname) {
+public class VpsReinstall extends ResponsableObject {
+    private final @NotNull String password;
+    private final @NotNull String hostname;
 
+    public VpsReinstall(@NotNull String password, @NotNull String hostname) {
+        this.password = password;
+        this.hostname = hostname;
+    }
+
+    @NotNull
+    public String getPassword() {
+        return password;
+    }
+
+    @NotNull
+    public String getHostname() {
+        return hostname;
+    }
 }
