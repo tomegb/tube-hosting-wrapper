d35ff2a295ce8768b0ccb803f702018ce7c22ca0
yazui <yazui.zag@gmail.com>
2022-08-31 23:43:13 +0200

    Update IpRDNSBody.java

--- src/main/java/com/tube/hosting/java/tube/objects/IpRDNSBody.java
+++ src/main/java/com/tube/hosting/java/tube/objects/IpRDNSBody.java
@@ -2,6 +2,17 @@ package com.tube.hosting.java.tube.objects;
 
 import org.jetbrains.annotations.NotNull;
 
-public record IpRDNSBody(@NotNull String rdns) {
+public class IpRDNSBody extends ResponsableObject {
+    private final @NotNull String rdns;
 
+    public IpRDNSBody(@NotNull String rdns) {
+        this.rdns = rdns;
+    }
+
+    @NotNull
+    public String getRdns() {
+        return rdns;
+    }
 }
+
+
