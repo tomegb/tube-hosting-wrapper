5bae4b5a94ccd68cf9c40e6826eb86e1d73056a3
yazui <yazui.zag@gmail.com>
2022-08-31 23:43:17 +0200

    Create PaymentOptions.java

---
+++ src/main/java/com/tube/hosting/java/tube/objects/PaymentOptions.java
@@ -0,0 +1,15 @@
+package com.tube.hosting.java.tube.objects;
+
+import org.jetbrains.annotations.NotNull;
+
+public enum PaymentOptions {
+    MANUALLY, PAYSAFECARD, CREDITCARD, PAYPAL, SOFORT, GIROPAY, BANKTRANSFER;
+
+
+    @NotNull
+    @Override
+    public String toString() {
+        return name().toLowerCase(); }
+
+
+}
