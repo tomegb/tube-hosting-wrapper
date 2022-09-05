5eb2d86868f5cc4f4fdd9555faea660a2d109bb7
yazui <yazui.zag@gmail.com>
2022-09-05 15:55:47 +0200

    Create PostUserPassword.java

---
+++ src/main/java/com/tube/hosting/java/tube/requests/post/me/controller/PostUserPassword.java
@@ -0,0 +1,12 @@
+package com.tube.hosting.java.tube.requests.post.me.controller;
+
+import com.tube.hosting.java.tube.objects.PlainMessage;
+import com.tube.hosting.java.tube.objects.UserChangePasswordObject;
+import com.tube.hosting.java.tube.requests.AbstractPostRequest;
+
+public class PostUserPassword extends AbstractPostRequest<PlainMessage, UserChangePasswordObject> {
+
+  public PostUserPassword(UserChangePasswordObject passwordChange) {
+    super("me/password", PlainMessage.class, passwordChange);
+  }
+}
