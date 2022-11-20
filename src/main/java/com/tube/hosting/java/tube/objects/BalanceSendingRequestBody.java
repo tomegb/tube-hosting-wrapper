c45069bd9e272fe1270d238e175a7bed1052e5e3
kxmpetentes <mikkakohnz@gmail.com>
2022-11-20 16:21:49 +0000

    Renamed BalanceSendingRequest to BalanceSendingRequestBody

--- src/main/java/com/tube/hosting/java/tube/objects/BalanceSendingRequestBody.java
+++ src/main/java/com/tube/hosting/java/tube/objects/BalanceSendingRequestBody.java
@@ -2,18 +2,16 @@ package com.tube.hosting.java.tube.objects;
 
 import org.jetbrains.annotations.NotNull;
 
-public class BalanceSendingRequest extends ResponsableObject {
+public class BalanceSendingRequestBody extends ResponsableObject {
 
   private final @NotNull String toMail;
   private final int amount;
   private final @NotNull String description;
-  private final int amountObject;
 
-  public BalanceSendingRequest(@NotNull String toMail, int amount, @NotNull String description, int amountObject) {
+  public BalanceSendingRequestBody(@NotNull String toMail, int amount, @NotNull String description) {
     this.toMail = toMail;
     this.amount = amount;
     this.description = description;
-    this.amountObject = amountObject;
   }
 
   @NotNull
@@ -30,7 +28,4 @@ public class BalanceSendingRequest extends ResponsableObject {
     return description;
   }
 
-  public int getAmountObject() {
-    return amountObject;
-  }
 }
